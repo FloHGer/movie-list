1686473c2ab7e2a130fcc3f35363b86606090a4f
FloHGer <80044261+FloHGer@users.noreply.github.com>
2021-10-11 15:54:38 +0200

    changed input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,21 +2,21 @@ import {useState} from 'react'
 import {useMovies} from '../contexts/MovieContext.jsx'
 
 export default function Input() {
-  const [movie, setMovie] = useState('');
-  const [movies, setMovies] = useMovies()
+  const [movie, setMovie] = useState({movieName:'', price:''});
+  const [movies, setMovies] = useMovies();
 
   const changeHandler = event => setMovie({...movie, [event.target.name]:event.target.value})
 
   const submitHandler = event => {
     event.preventDefault();
     setMovies([...movies, movie]);
-    setMovie('');
+    setMovie({movieName:'', price:''});
   }
   
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" name='movieName' onChange={changeHandler}/>
-      <input type="number" name='price' onChange={changeHandler}/>
+      <input type="text" name='movieName' value={movie.movieName} onChange={changeHandler}/>
+      <input type="number" name='price' value={movie.price} onChange={changeHandler}/>
       <input type="submit" value='SUBMIT'/>
     </form>
   )
